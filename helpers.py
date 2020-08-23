d1412facf4fc86759e3d16453a0728b1a26f9816
aln787 <aniderberg@gmail.com>
2020-08-23 07:43:50 -0400

    Adding the Quadratic Formula

--- helpers.py
+++ helpers.py
@@ -52,3 +52,16 @@ def fibonacci_sequence(n):
             if f <= n: print(f)
             else: break
 
+def quadratic_equation_example(a=88, b=100, c=20)
+"""Solve the quadratic equation ax**2 + bx + c = 0"""
+
+import math
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-math.sqrt(d))/(2*a)
+sol2 = (-b+math.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
